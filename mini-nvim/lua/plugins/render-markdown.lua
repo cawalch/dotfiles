2549ac32eb5afb99409c35625e0454f8ccf33781
Caleb Walch <cawalch@gmail.com>
2024-08-04 12:36:50 -0500

    more churn
    <!-- ps-id: 7dc0a715-9da9-4949-9d09-ed1730782dba -->

--- mini-nvim/lua/plugins/render-markdown.lua
+++ mini-nvim/lua/plugins/render-markdown.lua
@@ -1,11 +1,4 @@
-require("render-markdown").setup({
-  render_modes = { "n", "c", "i" },
-  bullets = { "➤" },
-  headings = { " ◉ ", " ○ ", " ✸ ", " ✿ " },
-  highlights = {
-    heading = {
-      backgrounds = { "CursorLine" },
-    },
-    code = "Ignore",
-  },
-})
+require("render-markdown").setup {
+  bullet = { enabled = true, icons = { "●", "○", "◆", "◇" } },
+  code = { enabled = true },
+}
